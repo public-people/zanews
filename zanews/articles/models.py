4eae8d94611ff947fafd01ce6eac6ecb4c4dc1d2
JD Bothma <jbothma@gmail.com>
2020-01-28 08:18:21 +0200

    Add models

--- zanews/articles/models.py
+++ zanews/articles/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django_extensions.db.models import TimeStampedModel
 
-# Create your models here.
+
+class Publication(TimeStampedModel):
+    name = models.CharField(max_length=200, unique=True)
+
+
+class Article(TimeStampedModel):
+    publication = models.ForeignKey(Publication, on_delete=models.CASCADE)
+    body_html = models.TextField(blank=True, help_text="The raw original HTML. Kept to be able to re-extract plain text if needed")
+    body_text = models.TextField(blank=True, db_index=True)
+    byline = models.TextField(bank=True)
+    file_name = models.TextField()
+    published_at = models.DateTimeField()
+    retrieved_at = models.DateTimeField()
+    spider_name = models.CharField(max_length=50)
+    title = models.CharField(max_length=300)
+    url = models.CharField(max_length=2048, unique=True)
