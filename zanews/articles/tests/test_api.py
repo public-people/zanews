fb20bf943d386e2bbddcaab1f3949724df5f97e7
JD Bothma <jbothma@gmail.com>
2020-02-01 09:52:29 +0200

    Add failing anon write rejection tests

--- zanews/articles/tests/test_api.py
+++ zanews/articles/tests/test_api.py
@@ -4,7 +4,7 @@ from rest_framework import status
 from zanews.articles import models
 
 
-class PublicationTests(APITestCase):
+class CreatePublicationTests(APITestCase):
     def test_create_publication(self):
         """
         Ensure we can create a new publication object.
@@ -18,6 +18,19 @@ class PublicationTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(models.Publication.objects.count(), 1)
         self.assertEqual(models.Publication.objects.get().name, "Wednesday Moon")
+
+    def test_anon_create_publication_rejected(self):
+        """
+        Ensure that AnonymousUser can not create a new publication object.
+        """
+        url = reverse("publication-list")
+        data = {
+            "name": "Wednesday Moon",
+            "slug": "whatever-we-want",
+        }
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(models.Publication.objects.count(), 0)
 
 
 class ArticleTests(APITestCase):
@@ -44,3 +57,25 @@ class ArticleTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(models.Article.objects.count(), 1)
         self.assertEqual(models.Article.objects.get().title, "It happened!")
+
+    def test_anon_create_article_rejected(self):
+        """
+        Ensure that AnonymousUser can not create a new article object.
+        """
+        url = reverse("article-list")
+        data = {
+            "publication": reverse(
+                "publication-detail", args=[models.Publication.objects.first().slug]
+            ),
+            "byline": "Fred Bloggs",
+            "body_html": "<p>A page body</p>",
+            "file_name": "it-happened.html",
+            "published_at": "2001-01-02T03:04:00",
+            "retrieved_at": "2002-03-04T05:06:00",
+            "spider_name": "wed_moon",
+            "title": "It happened!",
+            "published_url": "https://example.com/it-happened",
+        }
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(models.Article.objects.count(), 0)
